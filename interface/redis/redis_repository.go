c051f37b1369d146d257bdc34f395440e4b98c91
tozastation <b1016031@fun.ac.jp>
2019-05-11 23:58:50 +0900

    [Fix] fat model

---
+++ interface/redis/redis_repository.go
@@ -0,0 +1,121 @@
+package redis
+
+import (
+	"encoding/json"
+	"github.com/drill-shishamo-alliance/asotech_server/model/db"
+	"github.com/go-redis/redis"
+)
+
+type redisRepository struct {
+	*redis.Client
+}
+
+type IRedisRepository interface {
+	CreateUserRoom(userId, value string) (string, error)
+	CreateRoomMember(roomId string) error
+	CreateDemon(roomId, userId string) error
+	CreateUserLocation(userId string) error
+	DeleteUserLocation(userId string) error
+	GetRoomMember(roomId string) (*db.RoomUser, error)
+	GetDemonId(roomId string) (string, error)
+	GetUserLocation(userId string) (*db.UserLocation, error)
+	UpdateRoomMember(roomId string, value []byte) error
+}
+
+func NewRedisRepository(kvs *redis.Client) IRedisRepository {
+	return &redisRepository{kvs}
+}
+
+func (r *redisRepository) CreateUserRoom(userId, value string) (string, error) {
+	UserRoomKey := userId + "_ROOM"
+	UserRoomValue := value
+	// ルームキー
+	err := r.Client.Set(UserRoomKey, UserRoomValue, 0).Err()
+	if err != nil {
+		return "", err
+	}
+	return "", nil
+}
+
+func (r *redisRepository) CreateRoomMember(roomId string) error {
+	UserRoomMemberKey := roomId + "_MEMBER"
+	err := r.Client.Set(UserRoomMemberKey, "", 0).Err()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *redisRepository) CreateDemon(roomId, userId string) error {
+	UserRoomDemonKey := roomId + "_DEMON"
+	err := r.Client.Set(UserRoomDemonKey, userId, 0).Err()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *redisRepository) GetRoomMember(roomId string) (*db.RoomUser, error) {
+	UserRoomMemberKey := roomId + "_MEMBER"
+	roomUsersValue, err := r.Client.Get(UserRoomMemberKey).Result()
+	if err != nil {
+		return nil, err
+	}
+	// 2. 値を更新
+	roomUsers := db.RoomUser{}
+	err = json.Unmarshal([]byte(roomUsersValue), &roomUsers)
+	if err != nil {
+		return nil,err
+	}
+	return &roomUsers, nil
+}
+
+func (r *redisRepository) UpdateRoomMember(roomId string, value []byte) error {
+	UserRoomMemberKey := roomId + "_MEMBER"
+	err := r.Client.Set(UserRoomMemberKey, value, 0).Err()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *redisRepository) CreateUserLocation(userId string) error {
+	UserLocationKey := userId + "_LOCATION"
+	err := r.Client.Set(UserLocationKey, "", 0).Err()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func(r *redisRepository) DeleteUserLocation(userId string) error {
+	UserLocationKey := userId + "_LOCATION"
+	err := r.Client.Del(UserLocationKey).Err()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r redisRepository) GetDemonId(roomId string) (string, error) {
+	UserRoomDemonKey := roomId + "_DEMON"
+	userRoomDemonValue, err := r.Client.Get(UserRoomDemonKey).Result()
+	if err != nil {
+		return "", err
+	}
+	return userRoomDemonValue, nil
+}
+
+func (r *redisRepository) GetUserLocation(userId string) (*db.UserLocation, error) {
+	UserLocationKey := userId + "_LOCATION"
+	userLocationValue, err := r.Client.Get(UserLocationKey).Result()
+	if err != nil {
+		return nil, err
+	}
+	userLocation := db.UserLocation{}
+	err = json.Unmarshal([]byte(userLocationValue), &userLocation)
+	if err != nil {
+		return nil,err
+	}
+	return &userLocation, nil
+}
--- model/room.go
+++ model/room.go
@@ -3,50 +3,45 @@ package model
 import (
 	"encoding/json"
 	"github.com/drill-shishamo-alliance/asotech_server/interface/guid"
+	"github.com/drill-shishamo-alliance/asotech_server/interface/redis"
 	"github.com/drill-shishamo-alliance/asotech_server/model/db"
-	"github.com/go-redis/redis"
-	"strconv"
 )
 
 type room struct {
-	*redis.Client
+	redis.IRedisRepository
 	guid.IGuidUtil
 }
 
 type IRoom interface {
-	Insert(userId string, restrictTime int)
+	Insert(userId, restrictTime string, memberNum int) (string, error)
 	UpdateMember(roomId, userId string) (string, error)
 	SelectRoomMember(roomId string) (string, error)
-	UpdateMemberStatus(userId string) (string, error)
+	DecreaseMemberStatus(roomId, userId string) error
 	SelectRemainingHuman(roomId string) (string, error)
-	SelectHumansLocation(roomId string) (string, error)
-	SelectDemonsLocation(roomId string) (string, error)
+	SelectHumansLocation(roomId string) ([]*db.UserLocation, error)
+	SelectDemonsLocation(roomId string) (*db.UserLocation, error)
 }
 
-func NewRoom(kvs *redis.Client, id guid.IGuidUtil) IRoom {
-	return &room{kvs, id}
+func NewRoom(repo redis.IRedisRepository , id guid.IGuidUtil) IRoom {
+	return &room{repo, id}
 }
 
 // Insert
 func (r *room) Insert(userId, restrictTime string, memberNum int) (string, error) {
-	UserRoomKey := userId + "_ROOM"
-	UserRoomValue := r.IGuidUtil.CreateGuid()
-	// ルームキー
-	err := r.Client.Set(UserRoomKey, UserRoomValue, 0).Err()
+	// ゲームの部屋作成
+	UserRoomValue, err := r.IRedisRepository.CreateUserRoom(userId, r.IGuidUtil.CreateGuid())
 	if err != nil {
-		return "", err
+		return "", nil
 	}
 	// ゲームのメンバー
-	UserRoomMemberKey := UserRoomValue + "_MEMBER"
-	err = r.Client.Set(UserRoomMemberKey, "", 0).Err()
+	err =  r.IRedisRepository.CreateRoomMember(UserRoomValue)
 	if err != nil {
-		return "", err
+		return "", nil
 	}
 	// 鬼の決定
-	UserRoomDemonKey := UserRoomValue + "_DEMON"
-	err = r.Client.Set(UserRoomDemonKey, userId, 0).Err()
+	err = r.IRedisRepository.CreateDemon(UserRoomValue, userId)
 	if err != nil {
-		return "", err
+		return "", nil
 	}
 	return UserRoomValue, nil
 }
@@ -54,79 +49,54 @@ func (r *room) Insert(userId, restrictTime string, memberNum int) (string, error
 func (r *room) UpdateMember(roomId, userId string) (string, error) {
 	// 部屋にユーザを追加
 	// 1. ユーザのデータを取得
-	UserRoomMemberKey := roomId + "_MEMBER"
-	roomUsersValue, err := r.Client.Get(UserRoomMemberKey).Result()
+	roomMember, err := r.IRedisRepository.GetRoomMember(roomId)
 	if err != nil {
 		return "", err
 	}
-	// 2. 値を更新
-	roomUsers := db.RoomUser{}
-	err = json.Unmarshal([]byte(roomUsersValue), &roomUsers)
+	// 2. データ追加
+	roomMember.UserId = append(roomMember.UserId, userId)
+	roomMemberNewValue, err := json.Marshal(roomMember)
 	if err != nil {
-		return "", nil
-	}
-	roomUsers.UserId = append(roomUsers.UserId, userId)
-	roomUsersNewValue, err := json.Marshal(roomUsers)
-	if err != nil {
-		return "", nil
+		return "", err
 	}
 	// 3. 再保存
-	err = r.Client.Set(UserRoomMemberKey, roomUsersNewValue, 0).Err()
+	err = r.IRedisRepository.UpdateRoomMember(roomId, roomMemberNewValue)
 	if err != nil {
 		return "", err
 	}
 	// 4. ユーザの位置情報保存
-	UserLocationKey := userId + "_LOCATION"
-	err = r.Client.Set(UserLocationKey, "", 0).Err()
+	err = r.IRedisRepository.CreateUserLocation(userId)
 	if err != nil {
 		return "", err
 	}
 	return "", nil
 }
 
-func (r *room) SelectRoomMember(roomId string) (string, error){
-	// 1. ユーザのデータを取得
-	UserRoomMemberKey := roomId + "_ROOM"
-	roomUsersValue, err := r.Client.Get(UserRoomMemberKey).Result()
-	if err != nil {
-		return "", err
-	}
-	// 2. 値を更新
-	roomUsers := db.RoomUser{}
-	err = json.Unmarshal([]byte(roomUsersValue), &roomUsers)
-	if err != nil {
-		return "", nil
-	}
-	return string(len(roomUsers.UserId)), nil
+func (r *room) SelectRoomMember(roomId string) (string, error) {
+	return "", nil
 }
 
-func (r *room) UpdateMemberStatus(roomId, userId string) error {
+func (r *room) DecreaseMemberStatus(roomId, userId string) error {
 	// 1. メンバーを減らす
-	UserRoomMemberKey := roomId + "_MEMBER"
-	roomUsersValue, err := r.Client.Get(UserRoomMemberKey).Result()
+	roomMember, err := r.IRedisRepository.GetRoomMember(roomId)
 	if err != nil {
 		return err
 	}
 	// 2. 値を更新
-	roomUsers := db.RoomUser{}
-	err = json.Unmarshal([]byte(roomUsersValue), &roomUsers)
-	if err != nil {
-		return nil
-	}
 	result := db.RoomUser{}
-	for _, values := range roomUsers.UserId {
+	for _, values := range roomMember.UserId {
 		if values != userId {
 			result.UserId = append(result.UserId, values)
 		}
 	}
-	// 3. 再保存
-	err = r.Client.Set(UserRoomMemberKey, result, 0).Err()
+	// 3. 値を保存
+	roomMemberNewValue, err := json.Marshal(result)
+	err = r.IRedisRepository.UpdateRoomMember(roomId, roomMemberNewValue)
 	if err != nil {
 		return err
 	}
 	// 4. ロケーション消去
-	UserLocationKey := userId + "_LOCATION"
-	err = r.Client.Del(UserLocationKey).Err()
+	err = r.IRedisRepository.DeleteUserLocation(userId)
 	if err != nil {
 		return err
 	}
@@ -135,62 +105,71 @@ func (r *room) UpdateMemberStatus(roomId, userId string) error {
 
 func (r *room) SelectRemainingHuman(roomId string) (string, error) {
 	// 1. ルームのメンバーを取得
-	UserRoomMemberKey := roomId + "_MEMBER"
-	roomUsersValue, err := r.Client.Get(UserRoomMemberKey).Result()
+	roomMemberValue, err := r.IRedisRepository.GetRoomMember(roomId)
 	if err != nil {
 		return "", err
 	}
-	// 2. 鬼以外の生き残りを取得
-	roomUsers := db.RoomUser{}
-	err = json.Unmarshal([]byte(roomUsersValue), &roomUsers)
-	if err != nil {
-		return "", nil
-	}
 	// 2-1. 鬼のIDを取得
-	UserRoomDemonKey := roomId + "_DEMON"
-	userRoomDemonValue, err := r.Client.Get(UserRoomDemonKey).Result()
+	demonId, err := r.IRedisRepository.GetDemonId(roomId)
 	if err != nil {
 		return "", err
 	}
 	count := 0
-	for _, values := range roomUsers.UserId {
-		if values != userRoomDemonValue {
+	for _, values := range roomMemberValue.UserId {
+		if values != demonId {
 			count ++
 		}
 	}
 	return string(count), nil
 }
 
-func (r *room) SelectHumansLocation(roomId string) (string, error) {
+func (r *room) SelectHumansLocation(roomId string) ([]*db.UserLocation, error) {
 	// 1. ルームのメンバーを取得
-	UserRoomMemberKey := roomId + "_MEMBER"
-	roomUsersValue, err := r.Client.Get(UserRoomMemberKey).Result()
+	roomMemberValue, err := r.IRedisRepository.GetRoomMember(roomId)
 	if err != nil {
-		return "", err
-	}
-	// 2. 鬼以外の生き残りを取得
-	roomUsers := db.RoomUser{}
-	err = json.Unmarshal([]byte(roomUsersValue), &roomUsers)
-	if err != nil {
-		return "", nil
+		return nil, err
 	}
 	// 2-1. 鬼のIDを取得
-	UserRoomDemonKey := roomId + "_DEMON"
-	userRoomDemonValue, err := r.Client.Get(UserRoomDemonKey).Result()
+	demonId, err := r.IRedisRepository.GetDemonId(roomId)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 	// 3. 位置情報を取得
-	for _, value := range roomUsers.UserId {
-		UserLocationKey := value + "_LOCATION"
-		userLocation, err = r.Client.Get(UserLocationKey).Result()
-		if err != nil {
-			return "", err
+	var result []*db.UserLocation
+	for _, value := range roomMemberValue.UserId {
+		if value != demonId {
+			userLocation, err := r.IRedisRepository.GetUserLocation(value)
+			if err != nil {
+				return nil, err
+			}
+			result = append(result, userLocation)
 		}
 	}
-	return "", nil
+	return result, nil
 }
 
-func (r *room) SelectDemonsLocation(roomId string) (string, error) {
-	return "", nil
+func (r *room) SelectDemonsLocation(roomId string) (*db.UserLocation, error) {
+	// 1. ルームのメンバーを取得
+	roomMemberValue, err := r.IRedisRepository.GetRoomMember(roomId)
+	if err != nil {
+		return nil, err
+	}
+	// 2-1. 鬼のIDを取得
+	demonId, err := r.IRedisRepository.GetDemonId(roomId)
+	if err != nil {
+		return nil, err
+	}
+	// 3. 位置情報を取得
+	result := db.UserLocation{}
+	for _, value := range roomMemberValue.UserId {
+		if value == demonId {
+			userLocation, err := r.IRedisRepository.GetUserLocation(value)
+			if err != nil {
+				return nil, err
+			}
+			result = *userLocation
+			break
+		}
+	}
+	return &result, nil
 }
